a91cbf9d71905c4275d9f99854853acbff88b2fc
sujeong <tnwjd3730@naver.com>
2020-06-12 11:37:40 +0900

    Update Class comment

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -2,9 +2,8 @@ package Main;
 import Calculation.*;
 import java.util.Scanner;
 
-
 /**
- * 가격과 구작, 나이를 입력받고 할인 가격을 출력해 주는 메인 메소드
+ * Calculation 패키지의 할인 요금을 계산해주는 메소드를 실행시키는 메인 메소드가 담긴 클래스
  * 
  * 
  * @author (2017315002 김아름, 2017315038 이수정, 2018315051  야마모토 요시카) 
@@ -12,6 +11,10 @@ import java.util.Scanner;
  */
 public class MyApp
 {
+    /**
+     * 가격과 구작, 나이를 입력받고 할인된 가격을 출력해 주는 메인 메소드
+     * 
+     */ 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("가격을 입력해 주세요. >> ");
@@ -21,8 +24,8 @@ public class MyApp
         System.out.print("나이를 입력해 주세요. >> ");
         int age = scanner.nextInt();
         scanner.close();
+        
         DVDPriceCalculate calc = new DVDPriceCalculate();
-        double fee = calc.offThePrice(price, old, age);
-        System.out.println("할인 요금은 " + fee + "원입니다.");
+        System.out.println("할인 요금은 " + calc.offThePrice(price, old, age) + "원입니다.");
     }
 }
