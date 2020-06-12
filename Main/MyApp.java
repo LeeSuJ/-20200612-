3902d1a341e6e326e0da9f389d1f48a67bb3b775
Areum Kim <rladkfma2030@gmail.com>
2020-06-12 11:06:15 +0900

    메인 메소드를 작성하였습니다.

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -1,4 +1,6 @@
 package Main;
+import Calculation.*;
+import java.util.Scanner;
 
 
 /**
@@ -10,6 +12,15 @@ package Main;
 public class MyApp
 {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("가격을 입력해 주세요. >> ");
+        double price = scanner.nextDouble();
+        System.out.print("나온 지 몇 개월 된 작품인지 숫자만 입력해 주세요. (Ex: 6) >> ");
+        int old = scanner.nextInt();
+        System.out.print("나이를 입력해 주세요. >> ");
+        int age = scanner.nextInt();
+        scanner.close();
+        double fee = DVDPriceCalculate.offThePrice(price, old, age);
+        System.out.println("할인 요금은 " + fee + "원입니다.");
     }
 }
